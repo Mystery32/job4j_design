0cdea702c42ae16cdb8216a043d5331c55d4d029
Роман <mystery32@yandex.ru>
2022-07-24 23:54:20 +0300

    6. Кодировка.

--- src/main/java/ru/job4j/io/ConsoleChat.java
+++ src/main/java/ru/job4j/io/ConsoleChat.java
@@ -18,28 +18,29 @@ public class ConsoleChat {
     }
 
     public void run() {
+        List<String> phrases = readPhrases();
         List<String> log = new ArrayList<>();
         Scanner input = new Scanner(System.in);
+        var random = new Random();
         String phrase = "null";
         boolean active = true;
         System.out.println("Введите фразу:");
-        while (!phrase.equals(OUT)) {
+        while (!OUT.equals(phrase)) {
             phrase = input.nextLine();
-            if (phrase.equals(STOP)) {
+            log.add("Я: " + phrase);
+            if (STOP.equals(phrase)) {
                 active = false;
-                log.add("Я: " + phrase);
                 continue;
             }
-            if (phrase.equals(CONTINUE)) {
+            if (CONTINUE.equals(phrase)) {
                 active = true;
-                log.add("Я: " + phrase);
                 System.out.println("ПРОДОЛЖАЕМ!");
                 log.add("Бот: " + "ПРОДОЛЖАЕМ!");
-            } else {
-                log.add("Я: " + phrase);
-                if (!phrase.equals(OUT) && active) {
-                    log.add("Бот: " + randomPhrases());
-                }
+            }
+            if (active) {
+                var botPhrase = phrases.get(random.nextInt(phrases.size() - 1));
+                log.add("Бот: " + botPhrase);
+                System.out.println(botPhrase);
             }
             saveLog(log);
         }
@@ -53,42 +54,6 @@ public class ConsoleChat {
             e.printStackTrace();
         }
         return phrases;
-    }
-
-    private String randomPhrases() {
-        String botPhrase = null;
-        Map<String, String> map = new HashMap<>();
-        String[] array;
-        for (String s : readPhrases()) {
-            array = s.split("=", 2);
-            map.put(array[0], array[1]);
-        }
-        int randomNumber = new Random().nextInt(6);
-        if (randomNumber == 0) {
-            botPhrase = map.get("0");
-            System.out.println(map.get("0"));
-        }
-        if (randomNumber == 1) {
-            botPhrase = map.get("1");
-            System.out.println(map.get("1"));
-        }
-        if (randomNumber == 2) {
-            botPhrase = map.get("2");
-            System.out.println(map.get("2"));
-        }
-        if (randomNumber == 3) {
-            botPhrase = map.get("3");
-            System.out.println(map.get("3"));
-        }
-        if (randomNumber == 4) {
-            botPhrase = map.get("4");
-            System.out.println(map.get("4"));
-        }
-        if (randomNumber == 5) {
-            botPhrase = map.get("5");
-            System.out.println(map.get("5"));
-        }
-        return botPhrase;
     }
 
     private void saveLog(List<String> log) {
