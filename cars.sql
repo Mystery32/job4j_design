440d5b4fc6401a983691688bba7d663ef562b405
Роман <69649934+Mystery32@users.noreply.github.com>
2022-08-06 16:49:38 +0300

    Add files via upload

--- cars.sql
+++ cars.sql
@@ -1,1 +1,14 @@
-select * from cars;
+create table cars(
+	id serial primary key,
+	name varchar(255),
+	engine text,
+	power integer
+);
+
+insert into cars(name, engine, power) values('BMW', 'V6', '249');
+insert into cars(name, engine, power) values('Toyota', 'V4', '180');
+insert into cars(name, engine, power) values('Lada', 'V4', '105');
+
+update cars set name = 'Merсedes'
+
+delete from cars;
