7208ad1db3b55da899733f537b3950d22bc51b47
Роман <69649934+Mystery32@users.noreply.github.com>
2022-08-09 19:26:43 +0300

    Add files via upload

---
+++ employees.sql
@@ -0,0 +1,45 @@
+create table departments(
+    id serial primary key,
+    name varchar(255)
+);
+
+create table employees(
+    id serial primary key,
+    name varchar(255),
+    departments_id int references departments(id)
+);
+
+insert into departments(name) 
+values ('Экономический отдел'), ('Юридический отдел'), ('IT'), ('Отдел снабжения'), ('Проектный отдел');
+
+insert into employees(name, departments_id)
+values ('Мария', 1), ('Светлана', 1), ('Елена', 1);
+insert into employees(name, departments_id)
+values ('Андрей', 2), ('Ольга', 2);
+insert into employees(name, departments_id)
+values ('Илья', 3), ('Федор', 3);
+insert into employees(name, departments_id)
+values ('Марина', 4);
+insert into employees(name)
+values ('Артем');
+
+select * from employees e left join departments d on e.departments_id = d.id;
+
+select * from employees e right join departments d on e.departments_id = d.id;
+
+select * from employees e full join departments d on e.departments_id = d.id;
+
+select * from employees e cross join departments d;
+
+select d.name 
+from departments d
+left join employees e
+on e.departments_id = d.id
+where e.name is null
+group by d.name;
+
+select * from employees e left join departments d on e.departments_id = d.id;
+select * from departments d right join employees e on d.id = e.departments_id;
+
+select * from departments d left join employees e on d.id = e.departments_id;
+select * from employees e right join departments d on e.departments_id = d.id;
---
+++ teens.sql
@@ -0,0 +1,13 @@
+create table teens(
+    id serial primary key,
+    name varchar(255),
+	gender varchar(255)
+);
+
+insert into teens(name, gender) 
+values ('Иван', 'мужчина'), ('Федор', 'мужчина'), ('Оксана', 'женщина'), ('Виктория', 'женщина'), ('Дарья', 'женщина');
+
+select t1.name, t1.gender, t2.name, t2.gender
+from teens as t1
+cross join teens as t2
+where t1.name != t2.name;
