300e769b685ee898791fa96fb7a400b911829687
Роман <mystery32@yandex.ru>
2022-08-12 12:15:54 +0300

    0. JDBC

--- src/test/java/ru/job4j/io/ConfigTest.java
+++ src/test/java/ru/job4j/io/ConfigTest.java
@@ -19,11 +19,11 @@ public class ConfigTest {
 
     @Test
     public void whenAppProperties() {
-        String path = "./data/app.properties";
+        String path = "./src/main/resources/app.properties";
         Config config = new Config(path);
         config.load();
         assertThat(config.value("hibernate.dialect"), is("org.hibernate.dialect.PostgreSQLDialect"));
-        assertThat(config.value("hibernate.connection.url"), is("jdbc:postgresql://127.0.0.1:5432/trackstudio"));
+        assertThat(config.value("hibernate.connection.url"), is("jdbc:postgresql://localhost:5432/trackstudio"));
         assertThat(config.value("hibernate.connection.driver_class"), is("org.postgresql.Driver"));
         assertThat(config.value("hibernate.connection.username"), is("postgres"));
         assertThat(config.value("hibernate.connection.password"), is("password"));
