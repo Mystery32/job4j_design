e6fed0f42e9c43de19b300312212490d3f45b938
Роман <mystery32@yandex.ru>
2022-08-15 15:04:34 +0300

    Утверждения с исключениями

---
+++ src/test/java/ru/job4j/assertj/NameLoadTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.assertj;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class NameLoadTest {
+
+    @Test
+    void checkEmpty() {
+        NameLoad nameLoad = new NameLoad();
+        assertThatThrownBy(nameLoad::getMap)
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessageMatching("^.+")
+                .hasMessageContaining("no data");
+    }
+
+    @Test
+    void parseWithoutNames() {
+        NameLoad nameLoad = new NameLoad();
+        assertThatThrownBy(nameLoad::parse)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageMatching("^.+")
+                .hasMessageContaining("array is empty");
+    }
+}
