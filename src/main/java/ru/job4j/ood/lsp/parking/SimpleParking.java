11e802fac4e0582ce309d4bf6cdd489430d4ae0b
Роман <mystery32@yandex.ru>
2022-09-26 12:29:11 +0300

    2. Парковка машин

--- src/main/java/ru/job4j/ood/lsp/parking/SimpleParking.java
+++ src/main/java/ru/job4j/ood/lsp/parking/SimpleParking.java
@@ -3,12 +3,14 @@ package ru.job4j.ood.lsp.parking;
 import java.util.ArrayList;
 import java.util.List;
 
+import static ru.job4j.ood.lsp.parking.PassCar.CAR_SIZE;
+
 public class SimpleParking implements Parking {
 
     private final int passCarPlaces;
     private final int truckPlaces;
-    private final List<PassCar> passCars;
-    private final List<PassCar> trucks;
+    private final List<Car> passCars;
+    private final List<Car> trucks;
 
     public SimpleParking(int passCarPlaces, int truckPlaces) {
         this.passCarPlaces = passCarPlaces;
@@ -20,6 +22,16 @@ public class SimpleParking implements Parking {
     @Override
     public boolean add(Car car) {
         boolean result = false;
+        if (car.size <= CAR_SIZE && passCars.size() < passCarPlaces) {
+            passCars.add(car);
+            result = true;
+        } else if (car.size > CAR_SIZE && trucks.size() < truckPlaces) {
+            trucks.add(car);
+            result = true;
+        } else if (car.size <= passCarPlaces - passCars.size()) {
+            passCars.add(car);
+            result = true;
+        }
         return result;
     }
 }
--- src/test/java/ru/job4j/ood/lsp/parking/SimpleParkingTest.java
+++ src/test/java/ru/job4j/ood/lsp/parking/SimpleParkingTest.java
@@ -1,11 +1,9 @@
 package ru.job4j.ood.lsp.parking;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
 
-@Disabled
 class SimpleParkingTest {
 
     @Test
@@ -50,5 +48,14 @@ class SimpleParkingTest {
         assertThat(parking.add(passCar)).isTrue();
         assertThat(parking.add(truck1)).isTrue();
         assertThat(parking.add(truck2)).isFalse();
+    }
+
+    @Test
+    void whenParkingPassCarOnTruckPlace() {
+        Car passCar1 = new PassCar();
+        Car passCar2 = new PassCar();
+        Parking parking = new SimpleParking(1, 1);
+        assertThat(parking.add(passCar1)).isTrue();
+        assertThat(parking.add(passCar2)).isFalse();
     }
 }
